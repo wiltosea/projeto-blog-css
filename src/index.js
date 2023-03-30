166d11848fc0a24ea61ce464e51c5e91e33d12d4
wiltosea <wil@taprapeixe.com.br>
2023-03-30 09:50:30 -0300

    chave de acesso a api

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const client = new GraphQLClient({
   url: 'https://graphql.datocms.com/',
   headers: {
     // Replace this with your own read-only API token:
-    Authorization: 'Bearer b21d6c1fb8011c48fe88b5901a2519',
+    Authorization: 'Bearer a6619e22f88ae0d7455438c4b9e13c',
   },
 });
 
